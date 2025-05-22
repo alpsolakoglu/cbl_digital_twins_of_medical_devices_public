99697cce6ebe07bd51487ee86e63e11715a0c3bf
Alp Solakoglu <solakoglualp@yahoo.com>
2025-05-23 01:45:30 +0200

    Added slider controls for A, B, and C axes

--- src/main.py
+++ src/main.py
@@ -23,20 +23,35 @@ arm_id = p.loadURDF("/src/urdf/4dof.urdf", arm_start_pos, arm_start_orientation,
 
 # Add user sliders
 r_axis_slider = p.addUserDebugParameter("R-Axis Angle (deg)", 0, 180, 0)
+a_axis_slider = p.addUserDebugParameter("A-Axis Angle (deg)", -90, 90, 0)
+b_axis_slider = p.addUserDebugParameter("B-Axis Angle (deg)", 0, 160, 0)
+c_axis_slider = p.addUserDebugParameter("C-Axis Angle (deg)", 0, 160, 0)
 
 # Joint indices
+# Fixed joint index (not used in control)
+FIXED_JOINT_IDX = 1
+
 R_AXIS_JOINT_IDX = 0
-A_AXIS_JOINT_IDX = 1
-B_AXIS_JOINT_IDX = 2
-C_AXIS_JOINT_IDX = 3
+A_AXIS_JOINT_IDX = 2
+B_AXIS_JOINT_IDX = 3
+C_AXIS_JOINT_IDX = 4
 
 # Infinite simulation loop
 while True:
     r_axis_deg = p.readUserDebugParameter(r_axis_slider)
+    a_axis_deg = p.readUserDebugParameter(a_axis_slider)
+    b_axis_deg = p.readUserDebugParameter(b_axis_slider)
+    c_axis_deg = p.readUserDebugParameter(c_axis_slider)
 
     r_axis_rad = math.radians(r_axis_deg)
+    a_axis_rad = math.radians(a_axis_deg)
+    b_axis_rad = math.radians(b_axis_deg)
+    c_axis_rad = math.radians(c_axis_deg)
 
     p.setJointMotorControl2(arm_id, R_AXIS_JOINT_IDX, p.POSITION_CONTROL, targetPosition=r_axis_rad)
+    p.setJointMotorControl2(arm_id, A_AXIS_JOINT_IDX, p.POSITION_CONTROL, targetPosition=a_axis_rad)
+    p.setJointMotorControl2(arm_id, B_AXIS_JOINT_IDX, p.POSITION_CONTROL, targetPosition=b_axis_rad)
+    p.setJointMotorControl2(arm_id, C_AXIS_JOINT_IDX, p.POSITION_CONTROL, targetPosition=c_axis_rad)
 
     p.stepSimulation()
     time.sleep(time_step)
